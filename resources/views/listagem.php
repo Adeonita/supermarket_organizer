efbd7cd7bec93c6967e5327a61f4fdb5e07cea3d
adeonita <adeonita.sousa@gmail.com>
2020-02-09 00:44:22 -0300

    Exibiçao das categorias em formato de select

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -1,13 +1,35 @@
+<!--  EXIBIÇÃO DAS CATEGORIAS EM FORMATO DE TABELA
 <html>
     <body>
     <h1>Listagem de Categorias</h1>
     <table>
-        <?php foreach ($categories as $category): ?>
+        <?php //foreach ($categories as $category): ?>
         <tr>
-            <td><?print $category->name; ?> </td>
-            <td><?print  $category->description; ?> </td>
+            <td><? // print $category->id; ?> </td>
+            <td><? // print $category->name; ?> </td>
+            <td><? // print  $category->description; ?> </td>
         </tr>
-        <?php endforeach ?>
+        <?php //endforeach ?>
     </table>
     </body>
 </html>
+-->
+
+<!--eXIBIÇÃO DAS CATEGORIAS EM FORMATO DE SELECT-->
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Selecione uma categoria</h2>
+
+<label for="cars">Categorias:</label>
+
+<select >
+<?php foreach ($categories as $category): ?>
+  <option value="<?php $category->id ?>"> <?php print $category->name ?></option>
+<?php endforeach ?>
+  
+</select>
+  
+</body>
+</html>
+

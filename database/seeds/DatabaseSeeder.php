9865e5ca31d3dfb723def1b84bf05290e892dd75
adeonita <adeonita.sousa@gmail.com>
2020-02-08 23:55:20 -0300

    Ajustes de escopo e inserçao de rotas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
         $this->call('CategoryTableSeeder');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/list/categories', 'CategoriesController@list');
+
+Route::get('/login','LoginController@login');

a0f1f9d5ec3be216d3c4f6a6b34b69b7e031faed
adeonita <adeonita.sousa@gmail.com>
2020-02-09 00:31:41 -0300

    Separaçao das camadas, passando a responsabilidade de exibiçao para a view

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,13 +6,15 @@
     use App\Category;
 
     class CategoriesController extends Controller{
-        
-        public function list(){
-            $result = DB::connection('mysql')->select('select * from categories');
 
-            foreach ($result as $p) {  //Percorro o array produtos atribuindo cada ítem individual a p
-                print '<li>Nome: '. $p->name . ' - Descrição: '. $p->description . '</li><br>';  //Acesso os atributos nome e descrição de cada produto. Concatenando da variavel html
-            }
+        /**
+         * Função que acessa o banco, seleciona as categorias e as retorna para serem utilizadas na view
+         */
+        public function list(){
+
+            $categories = DB::connection('mysql')->select('select * from categories');
+
+            return view('listagem')->with('categories', $categories);  
         }
     }
 

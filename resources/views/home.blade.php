f719842c4be4ca45b1951a17fdc3d36fe6177100
adeonita <adeonita.sousa@gmail.com>
2020-02-08 23:52:24 -0300

    Alteraçoes na home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Supermarket Organizer</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body{
+                background-color: #fff;
+                font-family: 'Raleway', sans-serif;
+                height: 100vh;
+                color: #636b6f;
+                font-weight: 100;
+            }
+
+            .home{
+                color: white;
+                font-weight: bold;
+                background-image: url(https://pixabay.com/get/54e0d34b4850ac14f6d1867dda6d367b1436dce053576c4870277ad6974ac65eb0/salad-2068220_1920.jpg);
+                background-repeat: no-repeat;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height ">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Organizador de compras
+                </div>
+
+                <div class="links">
+                    <a href="https://localhost:8000/login">Login</a>
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

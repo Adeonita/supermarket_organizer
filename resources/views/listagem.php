252480b02d0ca1b23f91a2109ff9470e6fe89481
adeonita <adeonita.sousa@gmail.com>
2020-02-09 01:17:04 -0300

    Inclusao do css

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -18,18 +18,21 @@
 <!--eXIBIÇÃO DAS CATEGORIAS EM FORMATO DE SELECT-->
 <!DOCTYPE html>
 <html>
-<body>
-<h2>Selecione uma categoria</h2>
-
-<label for="cars">Categorias:</label>
-
-<select >
-<?php foreach ($categories as $category): ?>
-  <option value="<?php $category->id ?>"> <?php print $category->name ?></option>
-<?php endforeach ?>
-  
-</select>
-  
-</body>
+    <head>
+        <link href="/css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <h2>Selecione uma categoria</h2>
+    
+            <label for="cars">Categorias:</label>
+    
+            <select >
+                <?php foreach ($categories as $category): ?>
+                    <option value="<?php $category->id ?>"> <?php print $category->name ?></option>
+                <?php endforeach ?>
+            </select>
+        </div>
+    </body>
 </html>
 

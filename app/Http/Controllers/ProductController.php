0b686687b29feae6b87e96ba6086a64155369a6e
adeonita <adeonita.sousa@gmail.com>
2020-02-20 18:03:41 -0300

    Correçao

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,23 +3,30 @@
 
     use App\Http\Models\Product;
     use App\Http\Models\Category;
+    use App\Http\Request\Product;
     use Request;
 
     class ProductController extends Controller{
 
-        public function create(){
-            $categories = Category::all();
-            return view('product/form', ['categories' => $categories]);
+        public function store(RequestProduct $request){
+            $request = Product::all();
+            $response = json()->response($request);
+
+            return $response;
         }
 
-        public function add(){
-            $product = new Product();
-            $product->name = Request::input('name');
-            $product->price = Request::input('price');
-            $product->description = Request::input('description');
-            $product->category_id = Request::input('category_id');
+        public function update($id){
+            $request = Product::find($id));
+            $request->name = Request::input('name');
+            $request->price = Request::input('price');
+            $request->description = Request::input('description');
+            $request->category_id = Request::input('category_id');
             
-            $product->save();            
+            $request->save();  
+            $response = json()->response($request);
+
+            return $response;
+
         }
     }
 ?>

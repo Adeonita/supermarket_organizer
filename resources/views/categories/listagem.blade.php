46c1fbff9c223bf2e4da4a4d11e1be5687a1ae2d
adeonita <adeonita.sousa@gmail.com>
2020-02-20 17:45:56 -0300

    Remoçao das views

--- resources/views/categories/listagem.blade.php
+++
@@ -1,6 +0,0 @@
-<label for="category">Categoria:</label>
-<select id="category" name="category_id">
-@foreach ($categories as $category):
-    <option value=" {{$category->id}} "> {{$category->name}} </option>
-@endforeach
-</select>

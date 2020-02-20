1e37d5fd9afdddc32fa3931826c9c935288b452f
adeonita <adeonita.sousa@gmail.com>
2020-02-20 18:30:59 -0300

    Correçao dos retorno das funçoes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,30 +3,45 @@
 
     use App\Http\Models\Product;
     use App\Http\Models\Category;
-    use App\Http\Request\Product;
+    use App\Http\Request\ProductRequest;
     use Request;
 
     class ProductController extends Controller{
 
+        public function index(){
+            $request = Product::all();
+            $response = response()->json($request);
+
+            return $response;
+        }
+
         public function store(RequestProduct $request){
             $request = Product::all();
-            $response = json()->response($request);
+            $response = response()->json($request);
 
             return $response;
         }
 
         public function update($id){
-            $request = Product::find($id));
+            $request = Product::find($id);
             $request->name = Request::input('name');
             $request->price = Request::input('price');
             $request->description = Request::input('description');
             $request->category_id = Request::input('category_id');
             
             $request->save();  
-            $response = json()->response($request);
+            $response = response()->json($request);
 
             return $response;
+        }
 
+        public function show($id){
+            $request = Product::find($id);
+            $request = Product::all();
+            $response = response()->json($request);
+
+            return $request;
+        
         }
     }
 ?>

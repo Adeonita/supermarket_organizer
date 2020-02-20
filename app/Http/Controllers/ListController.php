00ac26aa53c3e10552d3feaa89926dbacdf66040
adeonita <adeonita.sousa@gmail.com>
2020-02-20 17:31:37 -0300

    Modificaçao das rotas

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -1,10 +1,23 @@
 <?php
     namespace App\Http\Controllers;
 
+    use Request;
+    use app\Http\Request\LitsRequest;
+    use app\Http\Models\List;
+
     class ListController extends Controller{
 
+        /**
+         * Function for create one list
+         */
         public function create(){
             return view('/list/form');
+        }
+
+        public function store(ListRequest $request){
+            $result = List::create($request->all());
+            $response = json->response($result);
+            return $response;
         }
 
         public function insert(){

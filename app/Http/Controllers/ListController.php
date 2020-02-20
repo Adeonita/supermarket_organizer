e64f8bbb603e87dfe5de1e71a23ce75fdad98349
adeonita <adeonita.sousa@gmail.com>
2020-02-19 21:20:17 -0300

    Implementaçao do metodo insert list

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -1,22 +1,45 @@
 <?php
     namespace App\Http\Controllers;
 
+    use App\Http\Models\List;
+    use Request;
+
     class ListController extends Controller{
 
         public function create(){
             return view('/list/form');
         }
 
-        public function insert(){
+        /**
+         * Função para inserir uma lista no banco de dados
+         * @param: user_id
+         * @return: Um reditect para minhas listas
+         * */
+        public function insert($user_id){
+            $list = new List();
+            $list->user_id = $user_id;
+            $list->name = Request::input('name');
+            $list->description = Request::input('description');
+            $list->user_id = Request::input('user_id');
+
+            $list->save();  //Essa chamada de método save substitui a query insert. Ela é herdada do model
+
+            return redirect()
+                ->action('/list')
+                ->witchInput(Request::only('name'));
+        }
+
+        public function update($id){
 
         }
 
-        public function update(){
-
-        }
-
-        public function delete(){
+        public function delete($id){
             
+        }
+
+        public function show($id){
+            $response = List::all();
+            return view('show', ['lists'] => $response);
         }
     }
 ?>

b80bf5a6f79d6649ea8f1baa6ece26386767b376
adeonita <adeonita.sousa@gmail.com>
2020-02-20 19:29:28 -0300

    Inserçao da propriedade fillable

--- app/Http/Models/List.php
+++ app/Http/Models/List.php
@@ -4,7 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class List extends Model
-{
-    //
+class List extends Model{
+
+    protected $table = "lists";
+    public $timestamp = false;
+    protected $fillable = array('name', 'description');
 }
--- app/Http/Models/Product.php
+++ app/Http/Models/Product.php
@@ -4,6 +4,9 @@ namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
+class Product extends Model{
+    
+    protected $table = "products";
+    public $timestamp = false;
+    protected $fillable = array('name', 'price', 'category_id', 'description', 'list_id');
 }
